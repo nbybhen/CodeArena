7ec34557a429406b66d388cb15f9b0d1003c57f9
ExplosiveEnd <henockdinberu11@gmail.com>
2024-05-17 05:51:37 -0400

    Created terminal and code editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
+import { XTerm } from 'xterm-for-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Editor} from '@monaco-editor/react';
+
+export default function App() {
+    let xTermRef = useRef(null);
+    let [input, setInput] = useState<string>("");
+    let [entries, setEntries] = useState([]);
+
+    useEffect(() => {
+        console.log("Input: ", input);
+        console.log("input charcode: ", input.charCodeAt(0));
+        }, [input, xTermRef]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div style={{"marginBottom": "50px"}}>
+                    <Editor height={"20vh"} width={"45vw"} defaultLanguage={"python3"} defaultValue={"print(\"Hello World!\")"}/>
+                    <button style={{width: "50px", height: "20px"}}>Run</button>
+                </div>
+                <XTerm ref={xTermRef}
+                       onData={(data: string) => {
+                           setInput(data);
+                           xTermRef.current.terminal.write(data);
+                       }}
+                       onKey={(event: { domEvent: { key: string; }; }) => {
+                           if(event.domEvent.key === "Backspace") {
+                               if(input) {
+                                   console.log("Input: ",input,"\nLength: ",input.length);
+                                   setInput(input.slice(0, input.length - 1));
+                                   xTermRef.current.terminal.write("\b \b");
+                               }
+                           }
+                           if(event.domEvent.key === "Enter") {
+                               if(input) {
+                                   setEntries(oldArray => [...oldArray, input]);
+                                   xTermRef.current.terminal.write("\r\n");
+                               }
+                           }
+                       }}
+
+                />
+                <div id="terminal"></div>
+            </header>
+        </div>
+    );
 }
 
-export default App;
