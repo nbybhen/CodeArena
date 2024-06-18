a3aad827b3a4514cf9f28873ce1b2292191bd1a7
ExplosiveEnd <henockdinberu11@gmail.com>
2024-06-17 22:43:38 -0400

    Session::term can be nullable TODO (soon): - Creating UI / components with NextJS - Adding compiled languages (Rust??) - Change positioning / sizing of terminal

--- api.ts
+++ api.ts
@@ -1,47 +1,49 @@
 import * as pty from "node-pty";
-import {IPty} from "node-pty";
+import { IPty } from "node-pty";
 
 let lang = {
-    "python": {
-        cmd : "python3",
-        args: "-c"
-    },
-    "rust": {
-        cmd: "rust-c",
-        args: ""
-    },
-    "javascript": {
-        cmd: "node",
-        args: "-e"
-    }
-}
+  python: {
+    cmd: "python3",
+    args: "-c",
+  },
+  rust: {
+    cmd: "rust-c",
+    args: "",
+  },
+  javascript: {
+    cmd: "node",
+    args: "-e",
+  },
+};
 
 export default class Session {
-    term: IPty;
-    socket: any;
-    code: string;
-    lang: string;
-    constructor(socket: any, code: string, lang: string) {
-        this.socket = socket;
-        this.code = code;
-        this.lang = lang;
-        this.term = null;
-        this.run()
+  term: IPty | null;
+  socket: any;
+  code: string;
+  lang: string;
+
+  constructor(socket: any, code: string, lang: string) {
+    this.socket = socket;
+    this.code = code;
+    this.lang = lang;
+    this.term = null;
+    this.run();
+  }
+
+  run(): void {
+    const current = lang[this.lang];
+    console.log("Current: ", current);
+    if (this.term) {
+      this.term.kill();
     }
-    run(): void {
-        const current = lang[this.lang];
-        console.log("Current: ",current);
-        if (this.term) {
-            this.term.kill();
-        }
-        console.log("This.code: ", this.code);
-        this.term = pty.spawn(current.cmd, [current.args, this.code], {
-            name: "xterm-color",
-            cwd: process.env.HOME,
-            env: process.env,
-        });
-        this.term.onData( (data) => {
-            this.socket.emit('response', JSON.stringify({message:"Terminal Output", output: data}));
-        });
-    }
+    console.log("This.code: ", this.code);
+    this.term = pty.spawn(current.cmd, [current.args, this.code], {
+      name: "xterm-color",
+      cwd: process.env.HOME,
+      env: process.env,
+    });
+    this.term.onData((data) => {
+      this.socket.emit("response", JSON.stringify({ message: "Terminal Output", output: data }));
+    });
+  }
 }
